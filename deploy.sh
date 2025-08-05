535d269951a9efd18d285fa6ba9ff9b46cec0155
Talor Itzhak <titzhak@redhat.com>
2025-08-05 18:13:55 +0300

    Custom kubelet with ocp/k8s PR#2366
    Signed-off-by: Talor Itzhak <titzhak@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 # Configure the variable below to point to an image registry
 # you have push permissions for and that will be accessible
 # from the cluster
-REGISTRY_IMAGE=quay.io/msivak/test-build:rhcos-layer-2
+REGISTRY_IMAGE=quay.io/titzhak/test-build:rhcos-layer-2
 
 #
 # Configure the MachineConfig label needed to targed the proper
 # MachineConfigPool
-MCLABEL="machineconfiguration.openshift.io/role: worker-cnf"
+MCLABEL="machineconfiguration.openshift.io/role: worker"
 
 #
 # Find the proper RHCOS base image for building the override
@@ -22,7 +22,7 @@ MCLABEL="machineconfiguration.openshift.io/role: worker-cnf"
 # 3. find Changes from / Changes / Components / Red Hat Enterprise Linux CoreOS upgraded from (A) to (B)
 # 4. Click (B) which will take you to the RHCOS release details page
 # 5. Copy the RHCOS Base OS url to the FROM below
-FROM="quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:da4c28471c05718a1e90879d5d932a9054a0e2e4248255bb079c58a3c13e58fe"
+FROM="quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:71a177fcc36bd91e20ac0414b081016a3f592387f57b4f39738928ea5046aed4"
 
 ### Do not change below this line
 
@@ -33,7 +33,7 @@ sed -e "s|^FROM quay.io/openshift-release.*$|FROM ${FROM}|" Containerfile.orig >
 podman build --no-cache -t ${REGISTRY_IMAGE} .
 podman push ${REGISTRY_IMAGE}
 
-# Configure the MachineConfig
+## Configure the MachineConfig
 sed -e "s|machineconfiguration.openshift.io/role.*|${MCLABEL}|" -e "s|osImageURL:.*|osImageURL: ${REGISTRY_IMAGE}|" mc-layer.yaml.orig  > mc-layer.yaml
 
 # Apply the MC to the cluster
